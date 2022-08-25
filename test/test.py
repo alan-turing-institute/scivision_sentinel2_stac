41d50b82bf6d28fd6a5f7a444546dff03f28f76f
Alejandro © <acocac@gmail.com>
2022-08-25 10:58:06 +0100

    add config

--- test/test.py
+++ test/test.py
@@ -1,8 +1,8 @@
-from scivision_sentinel2_stac import scivision_sentinel2_stac
+from scivision_sentinel2_stac import scivision_sentinel2_stac, cfgCollections
 
 # Settings
 collections = ["sentinel-s2-l2a-cogs"]
-res = 10
+res = 100
 bands = ["B04", "B03", "B02"]
 crs = "epsg:3857"
 groupby = "solar_day"
@@ -11,18 +11,7 @@ dates = ["2021-09-16"]
 centre_point = [113.887, -25.843]
 size = 6.5  # size of buffer
 
-# optional args
-# cfg = {
-#     "sentinel-s2-l2a-cogs": {
-#         "assets": {
-#             "*": {"data_type": "uint16", "nodata": 0},
-#             "SCL": {"data_type": "uint8", "nodata": 0},
-#             "visual": {"data_type": "uint8", "nodata": 0},
-#         },
-#         "aliases": {"red": "B04", "green": "B03", "blue": "B02"},
-#     },
-#     "*": {"warnings": "ignore"},
-# }
+cfg = cfgCollections(collections[0]).cfg()
 
 yy = scivision_sentinel2_stac.get_images(
         collections=collections,
